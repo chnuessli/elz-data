6f8d544a32769286e8cfcb812f9aec153cae9db1
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-11-01 09:58:52 +0100

    comment out other formats

--- converter.py
+++ converter.py
@@ -3,8 +3,8 @@ import pandas as pd
 df = pd.read_json (r'data\json\ambulance_station\ambulancestation_dispo_srz.geojson')
 df.to_csv (r'data/csv/ambulance_station/ambulancestation_dispo_srz.csv', index = None)
 
-df = pd.read_json (r'data\json\ambulance_station\ambulancestation_dispo_srz.geojson')
-df.to_excel (r'data/csv/ambulance_station/ambulancestation_dispo_srz.xlsx', index = None)
+#df = pd.read_json (r'data\json\ambulance_station\ambulancestation_dispo_srz.geojson')
+#df.to_excel (r'data/csv/ambulance_station/ambulancestation_dispo_srz.xlsx', index = None)
 
-df = pd.read_json (r'data\json\ambulance_station\ambulancestation_dispo_srz.geojson')
-df.to_xml (r'data/csv/ambulance_station/ambulancestation_dispo_srz.xml', index = None)
+#df = pd.read_json (r'data\json\ambulance_station\ambulancestation_dispo_srz.geojson')
+#df.to_xml (r'data/csv/ambulance_station/ambulancestation_dispo_srz.xml', index = None)
